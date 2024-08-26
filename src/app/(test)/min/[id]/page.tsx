1a167aa1b737dd7aa4645f9c4f4016efc0f0d25c
bang-wol <milk011922@gmail.com>
2024-08-26 10:19:41 +0000

    Chore: build.sh 파일 생성

--- src/app/(test)/min/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Pagination from '@/components/Pagination/Pagination';
-
-export default function Page({ params }: { params: { id: string } }) {
-  return (
-    <div>
-      <Pagination nowPage={Number(params.id)} totalPage={20} />
-    </div>
-  );
-}
